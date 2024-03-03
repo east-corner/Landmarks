9f17cbdd2e4c1154333282005fdae4e56211eb30
CharlieYu <yuhangbinccc@gmail.com>
2024-03-03 21:25:00 +0800

    feat: init

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 余杭斌 on 2024/3/3.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
